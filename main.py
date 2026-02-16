05991ed084530dcc7da4d7b6e082148e60ff8855
Brandon Williams <248243087+BrandWill-ML-DS-DE@users.noreply.github.com>
2026-02-15 21:29:03 -0600

    !

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ y = respondents['Adoption_Willingness']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Using Neural Networks (MLP) as requested in the Job Description
 clf = MLPClassifier(hidden_layer_sizes=(64, 32), max_iter=1000, random_state=42)
 clf.fit(X_train, y_train)
 
